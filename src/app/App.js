812f9d1d2f3030fb21be824ab3321a65af3d84b2
kocaman14 <o.kcmn@hotmail.com>
2024-12-20 22:18:50 +0300

    fıgmas

---
+++ src/app/App.js
@@ -0,0 +1,47 @@
+"use client"
+import "./globals.css"
+export default function App() {
+  const handleClick = () =>{
+console.log("merhaba")
+
+  }
+  return (
+    <div className="backGround">
+    <div className="textpart" style={{marginLeft:"20px",
+      marginTop:"180px"
+    }}> 
+     <div className="textbar">
+  <img src="/images/LOGIN.svg"  className="text"/>
+      </div>
+      <div style={{marginBottom:"10px"}}>
+<input placeholder="Username"  className="username"></input>
+      </div>
+      <div style={{marginBottom:"10px"}}>
+<input placeholder="Password" className="passwword"></input>
+      </div>
+     <img src="/images/LOGINBUTTON.svg"  className="logınbutton"  onClick={handleClick}/>
+     <div className="bottombar">
+<p className="forgotpass">Forgot Password</p>
+<p className="register">Register</p>
+    </div>
+    </div>
+    <div className="rightcorner">
+<img src="/images/LOGINHAMBURG.svg" className="hamburgpıcture"></img>
+<img src="/images/rectangle.svg"  className="ractangle"/>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,49 +1,12 @@
-"use client"
-import "./globals.css"
+'use client'
+import App from "./App"
 
-
-export default function Home() {
-  const handleClick = () =>{
-console.log("merhaba")
-
-  }
+const page = () => {
   return (
-    <div className="backGround">
-    <div className="textpart" style={{marginLeft:"20px",
-      marginTop:"180px"
-    }}> 
-     <div className="textbar">
-  <img src="/images/LOGIN.svg"  className="text"/>
-      </div>
-      <div style={{marginBottom:"10px"}}>
-<input placeholder="Username"  className="username"></input>
-      </div>
-      <div style={{marginBottom:"10px"}}>
-<input placeholder="Password" className="passwword"></input>
-      </div>
-     <img src="/images/LOGINBUTTON.svg"  className="logınbutton"  onClick={handleClick}/>
-     <div className="bottombar">
-<p className="forgotpass">Forgot Password</p>
-<p className="register">Register</p>
+    <div>
+      <App/>
     </div>
-    </div>
-    <div className="rightcorner">
-<img src="/images/LOGINHAMBURG.svg" className="hamburgpıcture"></img>
-<img src="/images/rectangle.svg"  className="ractangle"/>
-    </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-    </div>
-  );
+  )
 }
+
+export default page
